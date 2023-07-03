4a44ec7fc6506c07b03ae1ebe83810eb983dc5e9
lasaaliceblue <lasa@aliceblueindia.com>
2023-07-03 18:03:50 +0530

    update the pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Working</h1>
+    <div>
+        <multiselect v-model="selectedMechanic" id="mechanic_id" placeholder="Search mechanic" :options="mechanics"
+            label="name" track-by="id" @search-change="onSearchMechanicsChange" @input="onSelectedMechanic" />
+    </div>
+</template>
+
+<script>
+    import Multiselect from 'vue-multiselect';
+
+
+    export default {
+        components: {
+            Multiselect
+        },
+        props: {
+            mechanics: {
+                type: Array,
+                default: () => []
+            }
+        },
+        data() {
+            return {
+                selectedMechanic: undefined,
+                form: this.$inertia.form({
+                    mechanic_id: '',
+                    // other fields
+                })
+            }
+        },
+
+    }
+</script>
